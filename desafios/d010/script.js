1ddce9da93fb8fdfad4df18e09ff7e7826d2cb12
João Paulo <134535894+OJuauum@users.noreply.github.com>
2023-07-25 18:39:55 -0300

    d010

--- desafios/d010/script.js
+++ desafios/d010/script.js
@@ -6,5 +6,5 @@ function botao() {
 
     var delta = b**2-4*a*c
 
-    res.innerHTML = `<h2>Resolvendo Bhaskara</h2><p>A equação atual é ${b}x² + 6x + 2 = 0 <br> O cálculo realizado será Δ = ${b}² - 4.${a}.${c} <br> O valor calculado foi Δ = ${delta}</p>${delta}`
+    res.innerHTML = `<h2>Resolvendo Bhaskara</h2><p>A equação atual é ${a}x² + ${b}x + ${c} = 0 <br> O cálculo realizado será Δ = ${b}² - 4 . ${a} . ${c} <br> O valor calculado foi Δ = ${delta}</p>`
 }
