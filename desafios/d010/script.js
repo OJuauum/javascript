b456561cdaf01bfbbae6aa9991f6aacbc3154f0c
João Paulo <134535894+OJuauum@users.noreply.github.com>
2023-07-25 18:21:10 -0300

    d010

--- desafios/d010/script.js
+++ desafios/d010/script.js
@@ -1,3 +1,10 @@
 function botao() {
-    var a = prompt('')
+    var a = Number(prompt(`Qual é o valor de a?`))
+    var b = Number(prompt('Qual é o valor de b? '))
+    var c = Number(prompt('Qual é o valor de c?'))
+    var res = document.querySelector('div#res')
+
+    var delta = b**2-4*a*c
+
+    res.innerHTML = `<h2>Resolvendo Bhaskara</h2><p>A equação atual é ${b}x² + 6x + 2 = 0 <br> O cálculo realizado será Δ = ${b}² - 4.${a}.${c} <br> O valor calculado foi Δ = ${delta}</p>${delta}`
 }
