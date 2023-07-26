2966b86b3086cd9675afab63209865520c8c643b
João Paulo <134535894+OJuauum@users.noreply.github.com>
2023-07-26 09:44:07 -0300

    ex008

--- exercicios/ex008/ex008.js
+++ exercicios/ex008/ex008.js
@@ -1,1 +1,6 @@
-console.log ('O console funcionou corretamente!')
+var vel = 59
+console.log (`A velocidade do seu carro e de ${vel}Km/h`)
+if (vel > 60) {
+    console.log (`Você passou a velocidade permitida MULTADO`)
+}
+console.log (`Dirija usando cinto de segurança`)
