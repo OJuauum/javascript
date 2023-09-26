2176c5fba1e98674db58c352f0292d8b82b25121
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-26 09:05:03 -0300

    d006

--- desafios/d006/script.js
+++ desafios/d006/script.js
@@ -1,8 +1,8 @@
 function bot() {
-    var Celsius = Number(prompt(`Digite uma temperatura em °C (Celsius)`))
-    var res = document.getElementById('res')
-    var Kelvin = Celsius + 273
-    var Fahrenheit = Celsius * 1.8 + 32
+    const Celsius = Number(prompt(`Digite uma temperatura em °C (Celsius)`))
+    const res = document.getElementById('res')
+    const Kelvin = Celsius + 273
+    const Fahrenheit = Celsius * 1.8 + 32
 
     res.innerHTML = `<h2>A temperatura de ${Celsius.toLocaleString('pt-BR')}°C, corresponde a...</h2><p>${Kelvin.toLocaleString('pt-BR')}°K (Kelvin) <br> ${Fahrenheit.toLocaleString('pt-BR')}°F (Fahrenheit)</p>`
 }
