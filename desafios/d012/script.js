5d93e71194f4c4bc4f63feee8ce964a4d3375dac
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-26 09:16:06 -0300

    d012

--- desafios/d012/script.js
+++ desafios/d012/script.js
@@ -3,13 +3,13 @@ function botao() {
     const atupreço = Number(prompt(`Qual é o preço atual do produto?`))
     const res = document.querySelector('div#res')
     const preco = atupreço - antpreço
-    const vari = (((antpreço / atupreço)-1)*100)
+    const vari = (((antpreço / atupreço) - 1) * 100)
 
-    
+
 
     if (antpreço < atupreço) {
-         
-          res.innerHTML = `<h2>Analisando os valores informados</h2><p>O produto custava ${antpreço} e agora custa ${atupreço} <br> Hoje o produto está mais caro. <br> O preço subiu ${preco} em relação ao preço anterior. <br> Uma variação de ${vari}% pra cima.</p>`
+
+        res.innerHTML = `<h2>Analisando os valores informados</h2><p>O produto custava ${antpreço} e agora custa ${atupreço} <br> Hoje o produto está mais caro. <br> O preço subiu ${preco} em relação ao preço anterior. <br> Uma variação de ${vari}% pra cima.</p>`
     } else {
 
         res.innerHTML = `<h2>Analisando os valores informados</h2><p>O produto custava ${antpreço} e agora custa ${atupreço} <br> Hoje o produto está mais barato. <br> O preço caiu ${preco} em relação ao preço anterior. <br> Uma variação de ${vari}% pra baixo.</p>`
