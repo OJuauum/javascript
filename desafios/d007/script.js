6452a95b005f12ee09eae5cf0ac8122d814aa1d5
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-26 09:06:05 -0300

    d007

--- desafios/d007/script.js
+++ desafios/d007/script.js
@@ -1,8 +1,8 @@
 var Dolar = prompt(`Antes de mais nada. Quanto está a cotação do dólar agora?`)
 function bot() {
-    var Real = prompt(`Quantos R$ você tem na carteira? `)
-    var res = document.querySelector('div#res')
-    var convertido = Real / Dolar
+    const Real = prompt(`Quantos R$ você tem na carteira? `)
+    const res = document.querySelector('div#res')
+    const convertido = Real / Dolar
 
     res.innerHTML = `Você possui R$${Real} reias, que corresponde a US$${convertido.toFixed(2)} dolares.`
 }
