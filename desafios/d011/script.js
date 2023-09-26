4375906c69573c0be9fc683509e95746e8e9f4ec
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-26 09:14:12 -0300

    d011

--- desafios/d011/script.js
+++ desafios/d011/script.js
@@ -1,8 +1,8 @@
 function botao() {
-    var ano = Number(prompt(`Qual é o ano que você quer verificar?`))
-    var res = document.querySelector('div#res')
+    const ano = Number(prompt(`Qual é o ano que você quer verificar?`))
+    const res = document.querySelector('div#res')
 
-    if (ano % 4 == 0) { 
+    if (ano % 4 == 0) {
         res.innerHTML = `<h2>Analisando o ano de ${ano}...</h2><p>O ano de ${ano}  <mark id="bis">É BISSEXTO </mark> ✅</p>`
     } else {
         res.innerHTML = `<h2>Analisando o ano de ${ano}...</h2><p>O ano de ${ano} <mark id="nbis">NÃO É BISSEXTO </mark> ❌</p>`
