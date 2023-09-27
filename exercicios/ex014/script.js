d9b674e6cefda31e38ca6915e5bf98aad957cd52
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-27 16:48:50 -0300

    ex014

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,15 @@
-alert(`olá!`)
+function carregar() {
+    let msg = document.querySelector(`div#msg`)
+    let img = document.querySelector(`img#imagem`)
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = `pictures/manha.jpg`
+    } else if (hora >= 12 && hora < 18) {
+        img.src = `pictures/tarde.jpg`
+    } else {
+        img.src = `pictures/noite.jpg`
+    }
+}
+
