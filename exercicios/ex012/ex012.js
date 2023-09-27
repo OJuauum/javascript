13c796d036fe8d798c1a25cc126bb62a198d57aa
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-27 13:45:34 -0300

    ex012

--- exercicios/ex012/ex012.js
+++ exercicios/ex012/ex012.js
@@ -3,7 +3,7 @@ let hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
     console.log(`Bom dia!`)
-} else if (horas <= 18) {
+} else if (hora <= 18) {
     console.log(`Boa Tarde!`)
 } else {
     console.log(`Boa noite!`)
