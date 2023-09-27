3ac98069492c9a3adf7b86755ed61f8fe323ea1d
João Paulo <joaopaulodesouza6@gmail.com>
2023-09-27 16:56:21 -0300

    ex014

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -6,10 +6,13 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = `pictures/manha.jpg`
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         img.src = `pictures/tarde.jpg`
+        document.body.style.background = '#b9846f'
     } else {
         img.src = `pictures/noite.jpg`
+        document.body.style.background = '#515154'
     }
 }
 
