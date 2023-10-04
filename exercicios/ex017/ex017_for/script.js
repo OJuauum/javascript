426d7beb885cab5cd8d19703a13434687ee6d185
João Paulo <joaopaulodesouza6@gmail.com>
2023-10-04 12:33:42 -0300

    ex017

---
+++ exercicios/ex017/ex017_for/script.js
@@ -0,0 +1,17 @@
+function calcular() {
+    let num = document.querySelector(`input#txtn`)
+    let tab = document.querySelector(`select#seltab`)
+
+    if (num.value.length == 0) {
+        alert(`Por favor, digite um número!`)
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1;c <= 10; c++) {
+            let item = document.createElement(`option`)
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
